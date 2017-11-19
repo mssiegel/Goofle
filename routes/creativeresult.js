a806005173f086a9794d9ba2b3d4fd6be916eb32
Moshe Siegel <siegel.moshes@gmail.com>
2017-11-19 17:50:35 -0500

    added no sandbox to puppeteer for heroku

--- routes/creativeresult.js
+++ routes/creativeresult.js
@@ -21,7 +21,9 @@ router.post('/', upload, function (req, res, next) {
 
  
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
   const page = await browser.newPage();
   var inputName= req.body.inputName;
   
